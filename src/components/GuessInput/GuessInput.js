5d6c4390619c51a942afa47494f8979c6247c338
jasmine.pfingsten <jasmine.pfingsten@allianz.com>
2025-07-24 12:48:14 -0400

    Complete controlled input, challenge 1 complete

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -3,13 +3,19 @@ import React from 'react';
 function GuessInput() {
 
   const [guess, setGuess] = React.useState("");
-
-  console.log(guess);
   
   return (
-  <form class="guess-input-wrapper">
+  <form class="guess-input-wrapper" onSubmit={(event) => {
+        event.preventDefault();
+        if (guess.length < 5) {window.alert('Please submit a 5-letter word.'); return}
+        console.log(`guess: ${guess}`)
+        setGuess('');
+      }}>
     <label for="guess-input">Enter guess:</label>
-    <input id="guess-input" type="text" onChange={(event) => {setGuess(event.target.value)}} />
+      <input id="guess-input" type="text" maxlength="5" value={guess}  onChange={(event) => {
+        const currentGuess = event.target.value.toUpperCase();
+        setGuess(currentGuess);
+    }} />
   </form>);
 }
 
