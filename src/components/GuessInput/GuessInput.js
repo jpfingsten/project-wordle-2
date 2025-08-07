fdd95b985cad170770850878e1856f9ae797799f
jasmine.pfingsten <jasmine.pfingsten@allianz.com>
2025-08-07 12:19:37 -0400

    Update attributes to eliminate console warnings

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -5,13 +5,13 @@ function GuessInput() {
   const [guess, setGuess] = React.useState("");
   
   return (
-  <form class="guess-input-wrapper" onSubmit={(event) => {
+  <form className="guess-input-wrapper" onSubmit={(event) => {
         event.preventDefault();
         if (guess.length < 5) {window.alert('Please submit a 5-letter word.'); return}
         console.log(`guess: ${guess}`)
         setGuess('');
       }}>
-    <label for="guess-input">Enter guess:</label>
+    <label htmlFor="guess-input">Enter guess:</label>
       <input id="guess-input" type="text" pattern="[A-Za-z]{5}" title="Please enter a five-letter word" value={guess}  onChange={(event) => {
         const currentGuess = event.target.value.toUpperCase();
         setGuess(currentGuess);
