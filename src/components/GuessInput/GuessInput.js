3accaf32afff37a55fa79411155a198d5117135e
jasmine.pfingsten <jasmine.pfingsten@allianz.com>
2025-08-19 16:55:53 -0400

    Stop game from allowing more than 6 guesses

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -8,14 +8,20 @@ function GuessInput({guess, setGuess, guessList, setGuessList}) {
   <form className="guess-input-wrapper" onSubmit={(event) => {
         event.preventDefault();
         if (guess.length < 5) {window.alert('Please submit a 5-letter word.'); return}
-        setGuess('');
 
         const nextGuessesArray = [...guessesArray, guess];
         setGuessesArray(nextGuessesArray);
 
+        if (nextGuessesArray.length > 6) {
+          window.alert('You cannot make more than 6 guesses!');
+          nextGuessesArray.pop();
+          return
+        }
+
         const nextGuessList = [...guessList];
         nextGuessList.splice(0, nextGuessesArray.length, ...nextGuessesArray);
         setGuessList(nextGuessList);
+        setGuess('');
       }}>
     <label htmlFor="guess-input">Enter guess:</label>
       <input id="guess-input" type="text" pattern="[A-Za-z]{5}" title="Please submit a 5-letter word" value={guess}  onChange={(event) => {
